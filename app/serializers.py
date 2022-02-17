25b0c21f4337ebf295bafe363855a3e50207c307
juliacon-kul <ykulbulova@gmail.com>
2022-02-17 17:56:09 +0600

    Добавлен API метод post для element

--- app/serializers.py
+++ app/serializers.py
@@ -1,24 +1,16 @@
 from rest_framework import serializers
 from app.models import Element, ElementManager
 
-
-
 class ElementSerializer(serializers.ModelSerializer):
 
-    # parent = serializers.PrimaryKeyRelatedField(queryset=Element.objects.all())
-    # href = serializers.CharField(max_length=255)
-    # id = serializers.IntegerField(read_only=True)
-    # label = serializers.CharField(max_length=255)
-    # children = serializers.CharField(max_length=255)
+
     class Meta:
         model = Element
         fields = '__all__'
 
     def create(self, validated_data):
         element = Element.objects.create_element(**validated_data)
-
         return element
-
 
     def update(self, instance, validated_data):
         instance.label = validated_data.get('label', instance.label)
